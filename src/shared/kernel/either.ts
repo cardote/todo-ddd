4a6751786837c51ac45de34eeb10bdde9a0acaae
Edgar Cardote <edgar.akg@gmail.com>
2025-12-19 12:34:01 -0300

    feat(shared): add either.ts base class

---
+++ src/shared/kernel/either.ts
@@ -0,0 +1,41 @@
+// Left(L) = error
+// Right(R) = success
+// this is Left or Right -> Eliminated instanceof
+// Helper functions left() and right() avoid new Left() and new Right()
+
+export type Either<L, R> = Left<L, R> | Right<L, R>;
+
+export class Left<L, R> {
+  readonly value: L;
+
+  constructor(value: L) {
+    this.value = value;
+  }
+
+  isLeft(): this is Left<L, R> {
+    return true;
+  }
+
+  isRight(): this is Right<L, R> {
+    return false;
+  }
+}
+
+export class Right<L, R> {
+  readonly value: R;
+
+  constructor(value: R) {
+    this.value = value;
+  }
+
+  isLeft(): this is Left<L, R> {
+    return false;
+  }
+
+  isRight(): this is Right<L, R> {
+    return true;
+  }
+}
+
+export const left = <L, R>(value: L): Either<L, R> => new Left(value);
+export const right = <L, R>(value: R): Either<L, R> => new Right(value);
