18e9bc581844a775bf1f964e5927f841a62ddb2e
Edgar Cardote <edgar.akg@gmail.com>
2025-12-19 11:50:50 -0300

    feat(shared): add result.ts base class

---
+++ src/shared/kernel/result.ts
@@ -0,0 +1,45 @@
+export class Result<T> {
+  public readonly isFailure: boolean;
+  public readonly isSuccess: boolean;
+  public readonly error?: string;
+  public readonly _value?: T;
+
+  private constructor(isSuccess: boolean, error?: string, value?: T) {
+    // Success cannot have an error
+    if (isSuccess && error) {
+      throw new Error(
+        `Invalid operation: A result cannot be successful and contain an error`,
+      );
+    }
+    // Fail needs to have an error
+    if (!isSuccess && !error) {
+      throw new Error(
+        `Invalid operation: A failing result needs an error message`,
+      );
+    }
+
+    this.isSuccess = isSuccess;
+    this.isFailure = !isSuccess;
+    this.error = error;
+    this._value = value;
+  }
+
+  get value(): T {
+    // if try to access value without checking if it is success
+    if (!this.isSuccess) {
+      throw new Error(
+        'Cannot access value of a failure result. Use error instead.',
+      );
+    }
+
+    return this._value as T;
+  }
+
+  public static ok<T>(value: T): Result<T> {
+    return new Result<T>(true, undefined, value);
+  }
+
+  public static fail<T>(error: string): Result<T> {
+    return new Result<T>(false, error);
+  }
+}
