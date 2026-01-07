cb96d19f148763a74666afe84066b5aebd4f2bbe
Edgar Cardote <edgar.akg@gmail.com>
2026-01-07 14:36:51 -0300

    refactor(profile): update CreateProfileUseCase to return typed errors instead of generic Error

--- src/profile/application/use-cases/create-profile-use-case.ts
+++ src/profile/application/use-cases/create-profile-use-case.ts
@@ -4,6 +4,8 @@ import { ProfileName } from '@/profile/domain/value-objects/profile-name';
 import { Either, left, right } from '@/shared/kernel/either';
 import { EmailAlreadyInUseError } from '../errors/email-already-in-use-error';
 import { Profile } from '@/profile/domain/entities/profile';
+import { InvalidProfileNameError } from '../errors/invalid-profile-name-error';
+import { InvalidProfileEmailError } from '../errors/invalid-profile-email-error';
 
 type CreateProfileUseCaseInput = {
   name: string;
@@ -23,12 +25,12 @@ export class CreateProfileUseCase {
     // For example, check if email already exists, create profile entity, save to repository, etc.
     const name = ProfileName.create(input.name);
     if (name.isFailure) {
-      return left(new Error(name.error));
+      return left(new InvalidProfileNameError(name.error));
     }
 
     const email = ProfileEmail.create(input.email);
     if (email.isFailure) {
-      return left(new Error(email.error));
+      return left(new InvalidProfileEmailError(email.error));
     }
 
     const existingProfile = await this.profiles.findByEmail(email.value);
