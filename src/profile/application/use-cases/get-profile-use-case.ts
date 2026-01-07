1394b3bee7d0d767297fa06f0b127781389ca2d6
Edgar Cardote <edgar.akg@gmail.com>
2026-01-07 15:02:35 -0300

    feat(profile): implement GetProfile use case with not-found decision and typed error

---
+++ src/profile/application/use-cases/get-profile-use-case.ts
@@ -0,0 +1,39 @@
+import { ProfileRepository } from '@/profile/domain/repositories/profile-repository';
+import { ProfileId } from '@/profile/domain/value-objects/profile-id';
+import { Either, left, right } from '@/shared/kernel/either';
+import { ProfileNotFoundError } from '../errors/profile-not-found-error';
+
+type GetProfileUseCaseInput = {
+  profileId: string;
+};
+
+type GetProfileUseCaseOutput = {
+  profile: {
+    id: string;
+    name: string;
+    email: string;
+  };
+};
+export class GetProfileUseCase {
+  constructor(private readonly profiles: ProfileRepository) {}
+
+  async execute(
+    input: GetProfileUseCaseInput,
+  ): Promise<Either<Error, GetProfileUseCaseOutput>> {
+    const profile = await this.profiles.findById(
+      new ProfileId(input.profileId),
+    );
+
+    if (!profile) {
+      return left(new ProfileNotFoundError());
+    }
+
+    return right({
+      profile: {
+        id: profile.id.value,
+        name: profile.name.value,
+        email: profile.email.value,
+      },
+    });
+  }
+}
---
+++ src/tests/unit/profile/application/use-cases/get-profile-use-case.spec.ts
@@ -0,0 +1,40 @@
+import { ProfileNotFoundError } from '@/profile/application/errors/profile-not-found-error';
+import { GetProfileUseCase } from '@/profile/application/use-cases/get-profile-use-case';
+import { InMemoryProfileRepository } from '@/profile/infra/repositories/in-memory-profile-repository';
+import { makeTestProfile } from '@/tests/unit/shared/__tests__/utils/make-test-profile';
+
+describe('GetProfileUseCase (Unit)', () => {
+  it('should return profile successfully', async () => {
+    const repo = new InMemoryProfileRepository();
+    const useCase = new GetProfileUseCase(repo);
+    const { createProfile } = makeTestProfile();
+
+    const profile = createProfile();
+    await repo.save(profile);
+
+    const result = await useCase.execute({ profileId: profile.id.value });
+
+    expect(result.isRight()).toBe(true);
+
+    if (result.isRight()) {
+      expect(result.value.profile).toBeDefined();
+      expect(result.value.profile.id).toBe(profile.id.value);
+      expect(result.value.profile.name).toBe(profile.name.value);
+      expect(result.value.profile.email).toBe(profile.email.value);
+    }
+  });
+
+  it('should return error when profile not found', async () => {
+    const repo = new InMemoryProfileRepository();
+    const useCase = new GetProfileUseCase(repo);
+
+    const result = await useCase.execute({ profileId: 'any-id' });
+
+    expect(result.isLeft()).toBe(true);
+
+    if (result.isLeft()) {
+      expect(result.value).toBeInstanceOf(ProfileNotFoundError);
+      expect(result.value.message).toBe('Profile not found.');
+    }
+  });
+});
