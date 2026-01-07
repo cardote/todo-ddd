25845e034322453bd39435a75de221870cfe50f5
Edgar Cardote <edgar.akg@gmail.com>
2026-01-07 16:04:49 -0300

    feat(tasks): implement TaskId value-object

---
+++ src/tasks/domain/value-objects/task-id.ts
@@ -0,0 +1,5 @@
+import { UniqueEntityID } from '@/shared/kernel/unique-entity-id';
+
+export class TaskId extends UniqueEntityID {
+  // You can add any domain-specific methods or validations for TaskId here
+}
