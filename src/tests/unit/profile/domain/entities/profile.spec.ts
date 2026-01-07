a021d052a25d19a5ffbe57b4efc07139751d9170
Edgar Cardote <edgar.akg@gmail.com>
2026-01-07 12:01:16 -0300

    refactor(profile): update to use factory

--- src/tests/unit/profile/domain/entities/profile.spec.ts
+++ src/tests/unit/profile/domain/entities/profile.spec.ts
@@ -1,18 +1,11 @@
-import { ProfileEmail } from '@/profile/domain/value-objects/profile-email';
-import { ProfileName } from '@/profile/domain/value-objects/profile-name';
-import { Profile } from '@/profile/domain/entities/profile';
+import { makeTestProfile } from '@/tests/unit/shared/__tests__/utils/make-test-profile';
 
 const INITIAL_DATE = new Date('2026-01-01T00:00:00Z');
 const NEXT_DATE = new Date('2026-01-01T01:00:00Z');
-const makeProfile = () => {
-  const name = ProfileName.create('any-name').value;
-  const email = ProfileEmail.create('any-email@email.com').value;
 
-  return Profile.create({ name, email });
-};
 describe('Profile Entity (Unit)', () => {
   it('should set createdAt and updatedAt when not provided', () => {
-    const profile = makeProfile();
+    const profile = makeTestProfile().createProfile();
 
     expect(profile.createdAt).toBeInstanceOf(Date);
     expect(profile.updatedAt).toBeInstanceOf(Date);
@@ -22,12 +15,13 @@ describe('Profile Entity (Unit)', () => {
     vi.useFakeTimers();
     vi.setSystemTime(INITIAL_DATE);
 
-    const profile = makeProfile();
+    const { createProfile, createEmail } = makeTestProfile();
+    const profile = createProfile();
     const previousUpdatedAt = profile.updatedAt;
 
     vi.setSystemTime(NEXT_DATE);
 
-    const newEmail = ProfileEmail.create('new-email@email.com').value;
+    const newEmail = createEmail('new-email@email.com');
 
     profile.changeEmail(newEmail);
 
@@ -41,12 +35,13 @@ describe('Profile Entity (Unit)', () => {
     vi.useFakeTimers();
     vi.setSystemTime(INITIAL_DATE);
 
-    const profile = makeProfile();
+    const { createProfile, createName } = makeTestProfile();
+    const profile = createProfile();
     const previousUpdatedAt = profile.updatedAt;
 
     vi.setSystemTime(NEXT_DATE);
 
-    const newName = ProfileName.create('new-name').value;
+    const newName = createName('new-name');
 
     profile.changeName(newName);
 
