eba51344c7c49b16679354ce5d89504ab7630024
Edgar Cardote <edgar.akg@gmail.com>
2026-01-09 12:45:50 -0300

    fix(prisma): adjust prisma singleton implementation

--- src/shared/infra/prisma/prisma-client.ts
+++ src/shared/infra/prisma/prisma-client.ts
@@ -1,4 +1,12 @@
-import { PrismaClient } from '@prisma/client/extension';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+import { PrismaClient } from '../../../infra/database/generated/prisma/client';
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
 
 // Prisma singleton
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({ adapter });
